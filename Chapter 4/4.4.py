e147e691fce14b5d18a7ae42e6b0cf73e88c3ff7
ngonngupython <103424656+ngonngupython@users.noreply.github.com>
2022-07-08 23:19:10 +0700

    Add files via upload

---
+++ Chapter 4/4.4.py
@@ -0,0 +1,39 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Home page'
+
+
+@app.route('/user')
+def user():
+    return 'User page'
+
+
+@app.route('/about')
+def about():
+    return 'About page'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'Hello %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
+
+if __name__ == '__main__':
+    app.run()
